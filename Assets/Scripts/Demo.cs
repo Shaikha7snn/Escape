a2e072faf090f8f017488d8ba0323a46bc7f141c
EmanBareh <168358774+EmanBareh@users.noreply.github.com>
2024-05-30 13:10:59 +0300

    MainMenu+Level4
    implementation level 4 and main menu

---
+++ Assets/Scripts/Demo.cs
@@ -0,0 +1,15 @@
+
+using UnityEngine;
+
+public class Demo : MonoBehaviour
+{
+    [SerializeField] Timer timer1;
+    [SerializeField] LevelManager levelManager;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        timer1.SetDuration(60).Begin();
+        levelManager.timer = timer1; // Ensure the LevelManager knows about the timer
+    }
+}
